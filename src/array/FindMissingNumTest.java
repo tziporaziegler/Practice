fab7e3589663ed077b6e45d5922ebad886a2cc92
tziporaziegler <tziporaziegler@gmail.com>
2017-04-26 23:45:37 -0400

    Find missing num in array

---
+++ src/array/FindMissingNumTest.java
@@ -0,0 +1,60 @@
+package array;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FindMissingNumTest {
+
+	@Test
+	public void test52() {
+		int[] numbers = new int[99];
+
+		int number = 1;
+
+		for (int i = 0; i < 99; i++) {
+			numbers[i] = number;
+			number++;
+
+			if (number == 52) {
+				number++;
+			}
+		}
+
+		FindMissingNum missing = new FindMissingNum();
+
+		assertEquals(missing.missingNumRemove(numbers), 52);
+		assertEquals(missing.missingNumAdd(numbers), 52);
+	}
+
+	@Test
+	public void test1() {
+		int[] numbers = new int[99];
+
+		int number = 2;
+
+		for (int i = 0; i < 99; i++) {
+			numbers[i] = number;
+			number++;
+		}
+
+		FindMissingNum missing = new FindMissingNum();
+
+		assertEquals(missing.missingNumRemove(numbers), 1);
+		assertEquals(missing.missingNumAdd(numbers), 1);
+	}
+	
+	@Test
+	public void test100() {
+		int[] numbers = new int[99];
+
+		for (int i = 0; i < 99; i++) {
+			numbers[i] = i + 1;
+		}
+
+		FindMissingNum missing = new FindMissingNum();
+
+		assertEquals(missing.missingNumRemove(numbers), 100);
+		assertEquals(missing.missingNumAdd(numbers), 100);
+	}
+}
