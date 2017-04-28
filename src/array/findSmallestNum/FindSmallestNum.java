2282fefd1d86caeb177c57eaec4376a278ee3106
tziporaziegler <tziporaziegler@gmail.com>
2017-04-28 01:32:06 -0400

    Add FindSmallestNum

---
+++ src/array/findSmallestNum/FindSmallestNum.java
@@ -0,0 +1,22 @@
+package array.findSmallestNum;
+
+public class FindSmallestNum {
+
+	public int find(int[] array) throws Exception {
+		if (array != null) {
+			int smallest = array[0];
+
+			for (int i = 1; i < array.length; i++) {
+				int next = array[i];
+
+				if (next < smallest) {
+					smallest = next;
+				}
+			}
+
+			return smallest;
+		}
+
+		throw new Exception("Empty array");
+	}
+}
