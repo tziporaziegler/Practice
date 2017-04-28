f0e9133f569d83c8fc834a1051796e55ece77986
tziporaziegler <tziporaziegler@gmail.com>
2017-04-27 21:35:16 -0400

    Add CompareArrays

---
+++ src/array/compareArrays/CompareArrays.java
@@ -0,0 +1,53 @@
+package array.compareArrays;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/** Given two arrays, find which number(s) is not present in the second array.
+ * 
+ * @author Tzipora Ziegler
+ * 
+ */
+public class CompareArrays {
+
+	/**
+	 * @param array1 The first array of numbers
+	 * @param array2 The second array of numbers
+	 * @return The number from the second array that is
+	 * 		not present in the first array
+	 * @throws Exception Throws an exception if the missing number is not found
+	 */
+	public int FindMissingNumber(int[] array1, int[] array2) throws Exception {
+		Set<Integer> arraySet = new HashSet<Integer>();
+
+		for (int i : array2) {
+			arraySet.add(i);
+		}
+
+		for (int i : array1) {
+			if (!arraySet.contains(i)) {
+				return i;
+			}
+		}
+
+		throw new Exception("Number not found");
+	}
+	
+	public HashSet<Integer> FindMissingNumbers(int[] array1, int[] array2) {
+		Set<Integer> arraySet = new HashSet<Integer>();
+
+		for (int i : array2) {
+			arraySet.add(i);
+		}
+		
+		HashSet<Integer> missingNumbers = new HashSet<Integer>();
+
+		for (int i : array1) {
+			if (!arraySet.contains(i)) {
+				missingNumbers.add(i);
+			}
+		}
+
+		return missingNumbers;
+	}
+}
---
+++ src/array/compareArrays/CompareArraysTest.java
@@ -0,0 +1,35 @@
+package array.compareArrays;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashSet;
+
+import org.junit.Test;
+
+public class CompareArraysTest {
+
+	@Test
+	public void testMissingNumber() throws Exception {
+		int[] array1 = { 1, 2, 3, 4, 5 };
+		int[] array2 = { 2, 3, 1, 0, 5 };
+
+		CompareArrays compare = new CompareArrays();
+
+		assertEquals(compare.FindMissingNumber(array1, array2), 4);
+	}
+	
+	@Test
+	public void testMissingNumbers() {
+		int[] array1 = { 2, 3, 1, 0, 5, 0, 8 };
+		int[] array2 = { 1, 2, 3, 4, 5, 6, 7 };
+		
+		HashSet<Integer> missingNumbers = new HashSet<Integer>();
+		missingNumbers.add(0);
+		missingNumbers.add(8);
+		
+		CompareArrays compare = new CompareArrays();
+
+		assertEquals(compare.FindMissingNumbers(array1, array2), missingNumbers);
+	}
+
+}
